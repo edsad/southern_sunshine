4eabe27d592baf3587b330f75ec5bb02fca7361e
Ed Sadler <ed_lorie@comcast.net>
2017-12-20 16:21:52 -0600

    added more js

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,3 +6,9 @@ console.log("main js for southern sunshine is working");
 			var w = window.open("more-info5.html", "",
 		"width=1200,height=1000,left=300,top=200");
 		}
+
+	document.getElementById("openWindow6").onclick = openWindow6;
+		function openWindow6() {
+			var w = window.open("more-info6.html", "",
+		"width=1200,height=1000,left=300,top=200");
+		}
